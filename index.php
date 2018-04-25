af28ac1a7eaaac36553322fe6a06b1ae70142f6c
Julian <milch44@googlemail.com>
2018-04-25 15:27:20 +0200

    edited folder struct

--- index.php
+++ index.php
@@ -145,11 +145,12 @@
 </body>
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
-    <link rel="stylesheet" href="main.css">
+    <script src="js/app.js"></script>
+    <link rel="stylesheet" href="css/main.css">
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script src="app.js"></script>
+   
 
 </html>
