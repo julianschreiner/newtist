909a0d2415e6f86959f4bfbe49179a46ef4b8a86
Julian <milch44@googlemail.com>
2018-06-02 13:38:07 +0200

    web storage color mode

--- js/ui.js
+++ js/ui.js
@@ -13,6 +13,8 @@ $(".switch").find("input[type=checkbox]").on("change",function() {
         $('#txtNewReleases').css("color", "white");
         $('.select-dropdown').css("color", "white");
 
+        window.localStorage.setItem('colorMode', 'dark');
+
       }
       else{
         //whitemode
@@ -24,8 +26,39 @@ $(".switch").find("input[type=checkbox]").on("change",function() {
         $('.select-dropdown').css("color", "black");
 
 
+        window.localStorage.setItem('colorMode', 'white');
       }
   });
+
+  function switchColor(color){
+    if(color === "dark"){
+      //darkmode
+      $('body').css("background-color", "#263238");
+      $('#footer').css("background-color", "#263238");
+
+      /* TEXT */
+      $('#headlineNewtist').css("color", "white");
+      $('#txtSearchArtist').css("color", "white");
+      $('#txtNewReleases').removeClass('grey-text');
+      $('#txtNewReleases').css("color", "white");
+      $('.select-dropdown').css("color", "white");
+
+      window.localStorage.setItem('colorMode', 'dark');
+
+    }
+    else{
+      //whitemode
+      $('body').css("background-color", "white");
+
+      $('#headlineNewtist').css("color", "black");
+      $('#txtSearchArtist').css("color", "black");
+      $('#txtNewReleases').addClass('grey-text');
+      $('.select-dropdown').css("color", "black");
+
+
+      window.localStorage.setItem('colorMode', 'white');
+    }
+  };
 
 
   // FLOATING button
@@ -35,6 +68,15 @@ $(".switch").find("input[type=checkbox]").on("change",function() {
     var instances = M.FloatingActionButton.init(elems, {
       hoverEnabled: false
     });
+
+    /* STORAGE COLORMODE */
+    let colorMode = window.localStorage.getItem('colorMode');
+    let mode = '';
+
+    if(colorMode === 'dark') mode = 'dark';
+    else mode = 'white';
+
+    switchColor(mode);
   });
 
   // Or with jQuery
